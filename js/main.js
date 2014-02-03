f18ed908d10adbc450e965cc88d5fe2deb885ef0
Jess Jacobs <akisma@gmail.com>
2014-02-02 21:12:37 -0500

    update photos, create scroll waypoints, edit copy, add links

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(document).ready(function(){
 
+	$('.hands').waypoint(function(direction){
+		if (direction == 'down'){
+			$('.header-container').show();	
+			$('.mini-header').hide();
+		} else {
+			$('.header-container').hide();
+			$('.mini-header').show();
+		}
+	});
+});
